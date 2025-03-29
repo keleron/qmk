69be72480d142a7efbf72bfec6f87c92be7e0070
fabian.riquelme <fabian.riquelme@healthatom.com>
2025-03-29 00:23:10 -0300

    new commit

--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "keycodes.h"
+#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 
 // Left-hand home row mods
@@ -38,14 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define HOME_5 LCTL_T(KC_5)
 #define HOME_6 LSFT_T(KC_6)
 
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-
-// This globally defines all key overrides to be used
-// const key_override_t *key_overrides[] = {
-// 	&delete_key_override
-// };
-
-enum layer_names { _BASE, _MOVE, _SYMBOLS, _NUMBERS, _FN };
+enum layer_names { _BASE = SAFE_RANGE, _MOVE, _SYMBOLS, _NUMBERS, _FN };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
@@ -69,29 +62,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         XXXXXXX, XXXXXXX, KC_1, KC_2, KC_3, KC_0, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-        XXXXXXX, _______, KC_ENT, KC_SPC, MO(_FN), KC_DEL
+        XXXXXXX, _______, KC_ENT, KC_SPC, MO(_FN), XXXXXXX
         //`--------------------------'  `--------------------------'
         ),
     [_SYMBOLS] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_SLSH, LSFT(KC_SLSH), KC_BSPC,
+        KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, LSFT(KC_SLSH), XXXXXXX, KC_BSPC,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_TAB, XXXXXXX, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_EQL, KC_RPRN, KC_RBRC, KC_RCBR, XXXXXXX, KC_GRV,
+        KC_TAB, LSFT(KC_COMM), KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_PLUS, KC_RPRN, KC_RBRC, KC_RCBR, LSFT(KC_DOT), KC_GRV,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_UNDS, KC_PLUS, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD,
+        XXXXXXX, XXXXXXX, KC_TILD, KC_BSLS, KC_UNDS, XXXXXXX, XXXXXXX, KC_EQL, KC_SLSH, KC_PIPE, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-        KC_TAB, MO(_FN), KC_ENT, KC_SPC, OSL(_SYMBOLS), KC_DEL
-        //`----------------_----------'  `-------------------(pero que deberia hacer con esto [0] ///????)
+        XXXXXXX, MO(_FN), KC_ENT, KC_SPC, OSL(_SYMBOLS), XXXXXXX
+        //`----------------_----------'  `-------------------
         ),
     [_FN] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        QK_BOOT, KC_F1, KC_F2, XXXXXXX, KC_F4, KC_F5, KC_WSCH, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        QK_BOOT, KC_F1, KC_F2, XXXXXXX, KC_F4, KC_F5, KC_WSCH, XXXXXXX, KC_WFWD, XXXXXXX, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CW_TOGG, XXXXXXX, LCTL(KC_PGUP), KC_WBAK, KC_WFWD, LCTL(KC_PGDN), XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CW_TOGG, XXXXXXX, XXXXXXX, LCTL(KC_PGUP), KC_WBAK, LCTL(KC_PGDN), XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-        XXXXXXX, _______, KC_ENT, KC_SPC, _______, KC_DEL
+        XXXXXXX, _______, KC_ENT, KC_SPC, _______, XXXXXXX
         //`--------------------------'  `--------------------------'
         )
 };
